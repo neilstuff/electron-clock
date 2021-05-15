9f285adb08b24e631ff23237fe62b1ec20ca9962
Dr. Neil Brittliff <Neil.Brittliff@iinet.net.au>
2021-05-15 18:18:53 +1000

    First Commit

---
+++ main.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+var mainWindow = null;
+
+function createWindow() {
+
+    mainWindow = new BrowserWindow({
+        width: 110,
+        height: 110,
+        resizable: false,
+        frame: false,
+        transparent: true,
+        alwaysOnTop: true,
+        autoHideMenuBar: true,
+
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.setMenu(null);
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+    app.quit()
+})
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
